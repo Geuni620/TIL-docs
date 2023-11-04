0fe01f00223b342fd662ec40814f3fb4774a99ba
Geuni <dlrmsgnl620@naver.com>
2023-11-05 01:11:19 +0900

    template 정리

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import {Redirect} from "@docusaurus/router";
 export default function Home() {
   return (
     <Layout>
-      <Redirect to="/docs" />
+      <Redirect to="/docs/intro" />
     </Layout>
   );
 }
